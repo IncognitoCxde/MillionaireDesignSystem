0827fca19cb0e34cc65dad39412ee930f89dc546
IncognitoCxde <benissaeaston@gmail.com>
2025-11-21 16:57:47 +0500

    identification commit

--- Package.swift
+++ Package.swift
@@ -1,8 +1,33 @@
-// swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "MillionaireDesignSystem"
+    name: "MillionaireDesignSystem",
+    
+    platforms: [
+        .iOS(.v16)
+    ],
+    
+    products: [
+        .library(
+            name: "DesignSystem",
+            targets: ["DesignSystem"]
+        ),
+    ],
+    
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1")
+    ],
+    
+    targets: [
+        .target(
+            name: "DesignSystem",
+            dependencies: [
+                "SnapKit"
+            ],
+            path: "Sources/DesignSystem",
+            resources: [
+                .process("Resources"),
+            ]
+        ),
+    ]
 )
