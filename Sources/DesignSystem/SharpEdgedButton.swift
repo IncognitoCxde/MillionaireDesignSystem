49fbbdeeffb154f566e5fefcb161c61598c4d217
IncognitoCxde <benissaeaston@gmail.com>
2025-11-21 19:49:05 +0500

    button curve fix

--- Sources/DesignSystem/SharpEdgedButton.swift
+++ Sources/DesignSystem/SharpEdgedButton.swift
@@ -4,21 +4,45 @@ import SwiftUI
 
 public struct SlantedButtonShape: Shape {
     public func path(in rect: CGRect) -> Path {
-    var path = Path()
+        var path = Path()
 
-        let inset: CGFloat = rect.height * 0.3
-        let h = rect.height
-        let w = rect.width
+               let inset = rect.height * 0.32
+               let h = rect.height
+               let w = rect.width
 
-        path.move(to: CGPoint(x: inset, y: 0))
-        path.addLine(to: CGPoint(x: w - inset, y: 0))
-        path.addLine(to: CGPoint(x: w, y: h / 2))
-        path.addLine(to: CGPoint(x: w - inset, y: h))
-        path.addLine(to: CGPoint(x: inset, y: h))
-        path.addLine(to: CGPoint(x: 0, y: h / 2))
-        path.closeSubpath()
+               path.move(to: CGPoint(x: inset, y: 0))
 
-        return path
+               path.addQuadCurve(
+                   to: CGPoint(x: w - inset, y: 0),
+                   control: CGPoint(x: w / 2, y: -h * 0.18)
+               )
+
+               path.addQuadCurve(
+                   to: CGPoint(x: w, y: h / 2),
+                   control: CGPoint(x: w, y: h * 0.15)
+               )
+
+               path.addQuadCurve(
+                   to: CGPoint(x: w - inset, y: h),
+                   control: CGPoint(x: w, y: h * 0.85)
+               )
+
+               path.addQuadCurve(
+                   to: CGPoint(x: inset, y: h),
+                   control: CGPoint(x: w / 2, y: h + h * 0.18)
+               )
+
+               path.addQuadCurve(
+                   to: CGPoint(x: 0, y: h / 2),
+                   control: CGPoint(x: 0, y: h * 0.85)
+               )
+
+               path.addQuadCurve(
+                   to: CGPoint(x: inset, y: 0),
+                   control: CGPoint(x: 0, y: h * 0.15)
+               )
+
+               return path
     }
 }
 
