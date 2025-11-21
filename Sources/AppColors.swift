f9e538c3d01c34de7242962efbd5f14c1e09457c
IncognitoCxde <benissaeaston@gmail.com>
2025-11-21 16:54:59 +0500

    fix commit

--- Sources/AppColors.swift
+++ Sources/AppColors.swift
@@ -1,7 +1,18 @@
-//
-//  AppColors.swift
-//  MillionaireDesignSystem
-//
-//  Created by iMacbook on 11/21/25.
-//
+// App Colors
 
+import SwiftUI
+
+extension Color {
+    public static var customBlue: Color {
+        Color(named: "CustomBlue", in: .designSystemBundle, compatibleWith: nil)!
+    }
+    public static var customNavy: Color {
+        Color(named: "CustomNavy", in: .designSystemBundle, compatibleWith: nil)!
+    }
+    public static var darkBlue: Color {
+        Color(named: "DarkBlue", in: .designSystemBundle, compatibleWith: nil)!
+    }
+    public static var customTeal: Color {
+        Color(named: "CustomTeal", in: .designSystemBundle, compatibleWith: nil)!
+    }
+}
---
+++ Sources/AppIcons.swift
@@ -0,0 +1,14 @@
+//  App Icons
+
+import SwiftUI
+
+extension Image {
+    public static let appLogo = Image(named: "AppLogo", in: .designSystemBundle, compatibleWith: nil)!
+    public static let cashOut = Image(named: "CashOut", in: .designSystemBundle, compatibleWith: nil)!
+    public static let coin = Image(named: "Coin", in: .designSystemBundle, compatibleWith: nil)!
+    public static let help = Image(named: "Help", in: .designSystemBundle, compatibleWith: nil)!
+    public static let levels = Image(named: "Levels", in: .designSystemBundle, compatibleWith: nil)!
+    public static let like = Image(named: "Like", in: .designSystemBundle, compatibleWith: nil)!
+    public static let phone = Image(named: "Phone", in: .designSystemBundle, compatibleWith: nil)!
+    public static let timer = Image(named: "Timer", in: .designSystemBundle, compatibleWith: nil)!
+}
