cf599fc9396f76dc1d3402794ea079c01f515b26
IncognitoCxde <benissaeaston@gmail.com>
2025-11-24 19:47:57 +0500

    nav fix

--- Sources/DesignSystem/HomeNavBarView.swift
+++ Sources/DesignSystem/HomeNavBarView.swift
@@ -3,24 +3,22 @@ import SwiftUI
 public struct HomeNavBarView: View {
     
     var helpButtonAction: (() -> Void)?
-   
+    
     public init(helpButtonAction: (() -> Void)? = nil) {
         self.helpButtonAction = helpButtonAction
     }
     
     public var body: some View {
-        NavigationStack {
-            VStack {
-                
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        helpButtonAction?()
-                    }) {
-                        Image.help
-                            .padding(.trailing, 20)
-                    }
+        VStack {
+            
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {
+                    helpButtonAction?()
+                }) {
+                    Image.help
+                        .padding(.trailing, 20)
                 }
             }
         }
