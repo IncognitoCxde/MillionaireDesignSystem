29284cd82ff51c4e27438c2e230eeaf51fb1e660
IncognitoCxde <benissaeaston@gmail.com>
2025-11-28 11:28:22 +0500

    commit

--- Sources/DesignSystem/OptionsView.swift
+++ Sources/DesignSystem/OptionsView.swift
@@ -24,7 +24,6 @@ public struct AnswerOptionRow: View {
                     .foregroundColor(.darkGold)
                     .padding(.leading, 30)
                 
-                Spacer()
                 
                 Text(optionText)
                     .font(.system(size: 22, weight: .semibold))
