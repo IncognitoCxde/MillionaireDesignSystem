41be9ccb6d5b8310de2685480e91b6243125a684
IncognitoCxde <benissaeaston@gmail.com>
2025-11-30 10:44:32 +0500

    shine prize commit

--- Sources/DesignSystem/PrizeRow.swift
+++ Sources/DesignSystem/PrizeRow.swift
@@ -4,11 +4,13 @@ public struct PrizeRow: View {
     let leftText: String
     let rightText: String
     let gradient: LinearGradient
+    @Binding var shineEffect: CGFloat
 
-    public init(leftText: String, rightText: String, gradient: LinearGradient) {
+    public init(leftText: String, rightText: String, gradient: LinearGradient, shineEffect: Binding<CGFloat>?) {
         self.leftText = leftText
         self.rightText = rightText
         self.gradient = gradient
+        _shineEffect = shineEffect ?? .constant(0)
     }
 
     public var body: some View {
@@ -34,6 +36,15 @@ public struct PrizeRow: View {
                 .stroke(.white, lineWidth: 3)
         )
         .padding(.horizontal, 20)
+        .overlay(
+            LinearGradient(
+                colors: [Color.white.opacity(0.5), Color.clear, Color.white.opacity(0.5)],
+                startPoint: .topLeading,
+                endPoint: .topTrailing
+            )
+            .scaleEffect(x: 1 + shineEffect, y: 1, anchor: .leading)
+            .animation(.linear(duration: 1).repeatForever(autoreverses: true), value: shineEffect)
+        )
     }
 }
 
